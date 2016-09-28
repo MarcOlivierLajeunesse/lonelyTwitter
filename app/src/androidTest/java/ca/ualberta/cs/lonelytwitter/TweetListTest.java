6581db8313141ad9e0db5b5d2d0cf2746a5c741f
MarcOlivierLajeunesse <malajeun@ualberta.ca>
2016-09-27 19:35:02 -0600

    lab4 submission

--- app/src/androidTest/java/ca/ualberta/cs/lonelytwitter/TweetListTest.java
+++ app/src/androidTest/java/ca/ualberta/cs/lonelytwitter/TweetListTest.java
@@ -1,6 +1,12 @@
 package ca.ualberta.cs.lonelytwitter;
 
 import android.test.ActivityInstrumentationTestCase2;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+
 /**
  * Created by Marco on 27-Sep-2016.
  */
@@ -45,10 +51,55 @@ public class TweetListTest extends ActivityInstrumentationTestCase2{
         Tweet a = new NormalTweet("hello");
 
         list.add(a);
-        list.add(a);
         assertTrue(list.hasTweet(a));
 
-        list.delete(a);
+        list.removeTweet(a);
         assertFalse(list.hasTweet(a));
     }
+
+    public void testDuplicateTweet(){
+        TweetList list = new TweetList();
+
+        Tweet a = new NormalTweet("hello");
+        list.add(a);
+        try{
+            list.add(a);
+            fail();
+        } catch(IllegalArgumentException e){
+
+        }
+    }
+    public void testCount(){
+        TweetList list = new TweetList();
+
+        Tweet a = new NormalTweet("hello");
+        Tweet b = new NormalTweet("hi");
+
+        assertEquals(list.getCount(),0);
+        list.add(a);
+        assertEquals(list.getCount(),1);
+        list.add(b);
+        assertEquals(list.getCount(), 2 );
+    }
+    public void testGetTweetByDate(){
+        TweetList list = new TweetList();
+
+        Tweet a = new NormalTweet("hello", new Date(5000));
+        Tweet b = new NormalTweet("hi", new Date(20000));
+        Tweet c = new NormalTweet("boo!", new Date(30000));
+        list.add(b);
+        list.add(c);
+        list.add(a);
+
+        List<Tweet> timedTweets = new ArrayList<Tweet>();
+        timedTweets = list.getTweets();
+        assertEquals(timedTweets.get(0), a);
+        assertEquals(timedTweets.get(1), b);
+        assertEquals(timedTweets.get(2), c);
+        //check orriginal list
+        assertEquals(list.getTweet(0),b);
+        assertEquals(list.getTweet(1),c);
+        assertEquals(list.getTweet(2),a);
+    }
+
 }
--- app/src/main/java/ca/ualberta/cs/lonelytwitter/TweetList.java
+++ app/src/main/java/ca/ualberta/cs/lonelytwitter/TweetList.java
@@ -1,7 +1,11 @@
 package ca.ualberta.cs.lonelytwitter;
 
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
+
+import java.util.Date;
 
 /**
  * Created by Marco on 27-Sep-2016.
@@ -9,7 +13,12 @@ import java.util.List;
 public class TweetList {
     List<Tweet> tweets = new ArrayList<Tweet>();
     public void add(Tweet tweet) {
-        tweets.add(tweet);
+        if(this.hasTweet(tweet)) {
+            throw new IllegalArgumentException("duplicate tweet");
+        }
+        else
+            tweets.add(tweet);
+        
     }
 
     public boolean hasTweet(Tweet tweet) {
@@ -20,7 +29,24 @@ public class TweetList {
         return tweets.get(i);
     }
 
-    public void delete(Tweet a) {
+    public void removeTweet(Tweet a) {
         tweets.remove(a);
+    }
+
+
+    public int getCount() {
+        return tweets.size() ;
+    }
+
+    public List<Tweet> getTweets(){
+        List<Tweet> chronoTweets = new ArrayList<Tweet>(tweets);
+        //chronoTweets = tweets;
+        // comparator based on code from stackoverflow.com/questions/2784514/sort-arraylist-of-custom-objects-by-property
+        Collections.sort(chronoTweets, new Comparator<Tweet>() {
+            public int compare(Tweet lhs, Tweet rhs) {
+                return lhs.getDate().compareTo(rhs.getDate());
+            }
+        });
+        return chronoTweets;
     }
 }
